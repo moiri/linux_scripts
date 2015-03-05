64587ed69f8403301f2f3f73fe6a2e744c28e819
Simon Maurer <moirelein@gmail.com>
2015-03-05 12:37:10 +0000

    add template

---
+++ param.template.sh
@@ -0,0 +1,73 @@
+#!/bin/sh
+#http://agateau.com/2014/template-for-shell-based-command-line-scripts/
+set -e
+
+PROGNAME=$(basename $0)
+
+die() {
+    echo "$PROGNAME: $*" >&2
+    exit 1
+}
+
+usage() {
+    if [ "$*" != "" ] ; then
+        echo "Error: $*"
+    fi
+
+    cat << EOF
+
+Usage: $PROGNAME [OPTION ...] [foo] [bar]
+<Program description>.
+
+Options:
+-h, --help          display this usage message and exit
+-d, --delete        delete things
+-o, --output [FILE] write output to file
+
+EOF
+
+    exit 1
+}
+
+foo=""
+bar=""
+delete=0
+output="-"
+while [ $# -gt 0 ] ; do
+    case "$1" in
+        -h|--help)
+            usage
+            ;;
+        -d|--delete)
+            delete=1
+            ;;
+        -o|--output)
+            output="$2"
+            shift
+            ;;
+        -*)
+            usage "Unknown option '$1'"
+            ;;
+        *)
+            if [ -z "$foo" ] ; then
+                foo="$1"
+            elif [ -z "$bar" ] ; then
+                bar="$1"
+            else
+                usage "Too many arguments"
+            fi
+            ;;
+    esac
+    shift
+done
+
+if [ -z "$bar" ] ; then
+    usage "Not enough arguments"
+fi
+
+cat <<EOF
+foo=$foo
+bar=$bar
+delete=$delete
+output=$output
+EOF
