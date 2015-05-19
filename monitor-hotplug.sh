ef3f0b051313d6a113a8f0a36f4ab7c239e9dc03
Simon Maurer <moirelein@gmail.com>
2015-05-19 11:15:52 +0100

    set lxpanel position, fix after lubuntu upgrade (if root runs the script set variable if not, do nothing)

--- monitor-hotplug.sh
+++ monitor-hotplug.sh
@@ -2,23 +2,21 @@
 #initially created by codingtony (https://github.com/codingtony/udev-monitor-hotplug)
 
 #Adapt this script to your needs.
-
 DEVICES=$(find /sys/class/drm/*/status)
 
 #inspired by /etc/acpd/lid.sh and the function it sources
 
+# debug=/home/moiri/monitor.log
 displaynum=`ls /tmp/.X11-unix/* | sed s#/tmp/.X11-unix/X##`
 display=":$displaynum"
 export DISPLAY=":$displaynum"
 
-# uid=$(ck-list-sessions | awk 'BEGIN { unix_user = ""; } /^Session/ { unix_user = ""; } /unix-user =/ { gsub(/'\''/,"",$3); unix_user = $3; } /x11-display = '\'$display\''/ { print unix_user; exit (0); }')
-# if [ -n "$uid" ]; then
+xuid=$(xhost | sed s#SI:localuser:## | tail -1)
+uid=$(whoami)
+if [ "$uid" == "root" ]; then
 	# from https://wiki.archlinux.org/index.php/Acpid#Laptop_Monitor_Power_Off
-	export XAUTHORITY=$(ps -C Xorg -f --no-header | sed -n 's/.*-auth //; s/ -[^ ].*//; p')
-# else
-  # echo "unable to find an X session"
-  # exit 1
-# fi
+  export XAUTHORITY=$(ps -C Xorg -f --no-header | sed -n 's/.*-auth //; s/ -[^ ].*//; p')
+fi
 
 
 #this while loop declare the $HDMI1 $VGA1 $LVDS1 and others if they are plugged in
@@ -56,15 +54,24 @@ elif [ ! -z "$HDMI1" -a -z "$VGA1" ]; then
   xrandr --output eDP1 --off
   xrandr --output VGA1 --off
   xrandr --output HDMI1 --mode 1920x1200 --primary
+  # set lxpanel position
+  sed -i 's/monitor=1/monitor=0/g' /home/$xuid/.config/lxpanel/Lubuntu/panels/panel
+  lxpanelctl restart
 elif [ -z "$HDMI1" -a ! -z "$VGA1" ]; then
   echo "VGA1 is plugged in, but not HDMI1"
   xrandr --output eDP1 --mode 1920x1080 --noprimary
   xrandr --output HDMI1 --off
   xrandr --output VGA1 --mode 1920x1080 --right-of eDP1 --primary
+  # set lxpanel position
+  sed -i 's/monitor=0/monitor=1/g' /home/$xuid/.config/lxpanel/Lubuntu/panels/panel
+  lxpanelctl restart
 else
   echo "No external monitors are plugged in"
   xrandr --output VGA1 --off
   xrandr --output HDMI1 --off
   xrandr --output eDP1 --mode 1920x1080 --primary
+  # set lxpanel position
+  sed -i 's/monitor=1/monitor=0/g' /home/$xuid/.config/lxpanel/Lubuntu/panels/panel
+  lxpanelctl restart
 fi
 
